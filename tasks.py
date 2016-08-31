48d426d3475d47f4f0610e33bde5f96fee4652c1
Luc Saffre <luc.saffre@gmail.com>
2016-08-31 18:21:38 +0300

    http://luc.lino-framework.org/blog/2016/0831.html

--- tasks.py
+++ tasks.py
@@ -1,4 +1,13 @@
 # from atelier.invlib import add_demo_project
 from atelier.tasks import ns
-ns.setup_from_tasks(globals(), "lino_presto")
-ns.configure(dict(languages="en de fr et".split()))
+ns.setup_from_tasks(
+    globals(), "lino_presto",
+    languages="en de fr et".split(),
+    tolerate_sphinx_warnings = False,
+    blogref_url='http://luc.lino-framework.org',
+    revision_control_system='git',
+    locale_dir='lino_presto/lib/presto/locale',
+    cleanable_files=['docs/api/lino_presto.*'],
+    demo_projects=[
+        'lino_presto.projects.std.settings.demo',
+        'lino_presto.projects.psico.settings.demo'])
